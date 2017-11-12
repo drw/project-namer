0c41cad3aef7ebcfdc68d60def9310554aad9caf
drw <drw@users.noreply.github.com>
2017-11-12 15:51:53 -0500

    Factor API access into a separate function

--- namer.py
+++ namer.py
@@ -7,19 +7,30 @@ def combine(aggregation,element):
         return(element)
     return "{}-{}".format(aggregation,element)
 
+def obtain(endpoint,seeds):
+    xs = []
+    x_data = []
+    for seed in seeds:
+        seed_parts = seed.split(' ')
+        escape_seed = "+".join(seed_parts)
+        r = requests.get(datamuse+"/words?{}={}".format(endpoint,escape_seed))
+        xs.append([e['word'] for e in r.json()])
+        x_data.append(r.json())
+    return xs, x_data
+
+# Take all command-line arguments after the script
+# name as words/phrases that are inputs into making
+# a project name.
+# Currently, if n arguments are given, the suggested project
+# names are all n words (or n phrases) long.
 seeds = sys.argv[1:]
 
 datamuse = "https://api.datamuse.com"
-synonyms = []
-synonym_parameters = []
-for seed in seeds:
-    seed_list = seed.split(' ')
-    r = requests.get(datamuse+"/words?ml={}".format("+".join(seed_list)))
-    synonyms.append([e['word'] for e in r.json()])
-    synonym_parameters.append(r.json())
+synonyms, synonym_data = obtain('ml',seeds)
 
+limit = 10
 for i,seed in enumerate(seeds):
-    for synonym_i in synonyms[i][0:5]:
+    for synonym_i in synonyms[i][0:limit]:
         name = ""
         for j,seed in enumerate(seeds):
             if i != j:
