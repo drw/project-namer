f1e836fd0ef2114a9b7d9977d5d5f1db526f1a30
drw <drw@users.noreply.github.com>
2017-11-14 08:15:51 -0500

    Add common word pairs (based around rhymes but substituting the original word for the rhyming word)

--- namer.py
+++ namer.py
@@ -1,6 +1,7 @@
 import re,sys
 
 import requests
+from pprint import pprint
 
 def combine(aggregation,element):
     if aggregation == "":
@@ -18,6 +19,17 @@ def obtain(endpoint,seeds):
         x_data.append(r.json())
     return xs, x_data
 
+def remove_common_words(xs):
+    common_words = ['the', 'a', 'these', 'of', 'in', 'and', 'or', 'with']
+    common_words += ['such', 'his', 'to', 'those', 'this', 'that', 'is']
+    common_words += ['it', 'you', 'he', 'she', 'we', 'his', 'they', 'be']
+    common_words += ['by', 'for', 'not', 'shall', 'may', 'can', 'will']
+    common_words += ['would', 'could', 'might', 'must', 'should', 'much']
+    common_words += ['nor', 'does', 'as', 'from', 'on', 'than', 'at']
+    common_words += ['their', 'my', 'our', 'her', 'hers', 'him', 'its']
+    common_words += ['was', 'were', 'been', 'had', 'each', 'one', 'your']
+    common_words += ['have', 'who', 'are', 'into', 'whose', 'an']
+    return [x for x in xs if x not in common_words]
 # Take all command-line arguments after the script
 # name as words/phrases that are inputs into making
 # a project name.
@@ -27,14 +39,39 @@ seeds = sys.argv[1:]
 
 datamuse = "https://api.datamuse.com"
 synonyms, synonym_data = obtain('ml',seeds)
+rhymes, rhyme_data = obtain('rel_rhy',seeds)
+
+# If any of the rhymes are among any of the synonyms,
+# that results should immediately be promoted to the
+# top of the heap.
 
 limit = 10
+print("======== SYNONYM-BASED NAMES ========")
 for i,seed in enumerate(seeds):
-    for synonym_i in synonyms[i][0:limit]:
+    for word_i in synonyms[i][0:limit]:
         name = ""
         for j,seed in enumerate(seeds):
             if i != j:
                 name = combine(name,seed)
             else:
-                name = combine(name,synonym_i)
+                name = combine(name,word_i)
         print(name)
+
+print("========= RHYME-BASED NAMES =========")
+for i,seed in enumerate(seeds):
+    # For each of the first [limit] rhymes, generate a list of 
+    # words that tend to come before that rhyme.
+    rhyme_predecessors,predecessor_data = obtain('rel_bgb',rhymes[i][0:limit])
+    rhyme_predecessors = [remove_common_words(xs) for xs in rhyme_predecessors]
+    #pprint(rhyme_predecessors)
+
+    rhyme_limit = int(round(limit/4.0))
+    for k in range(0,len(rhyme_predecessors)):
+        for pred_k in rhyme_predecessors[k][0:rhyme_limit]:
+            name = ""
+            for j,seed in enumerate(seeds):
+                if i != j:
+                    name = combine(name,seed)
+                else:
+                    name = combine(combine(name,pred_k),seed)
+            print(name)
